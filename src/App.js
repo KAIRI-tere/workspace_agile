fe8f3cbe710be16d3af316ed19a408abe1ef7191
KAIRI-tere <takimoto.kairi.4rf@jp.nssol.nipponsteel.com>
2024-11-21 16:51:12 +0900

    とりあえず共有

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hogehoge</h1>
-      <ul>
-        <li>デザイン Lv. 2 0/2</li>
-        <li>マネジメント Lv. 4 3/5</li>
-        <li>ビジネス Lv. 5 0/8</li>
-        <li>その他 Lv. 7 1/21</li>
-      </ul>
+    <div className="App-main">
+      <div className="App-Box">
+        <div className="Prof">
+          <ul>
+              <li>デザイン Lv. 2 0/2</li>
+              <li>マネジメント Lv. 4 3/5</li>
+              <li>ビジネス Lv. 5 0/8</li>
+              <li>その他 Lv. 7 1/21</li>
+            </ul>
+        </div>
+        <div className="Book-Level">
+          <ul>
+            <li>デザイン Lv. 2 0/2</li>
+            <li>マネジメント Lv. 4 3/5</li>
+            <li>ビジネス Lv. 5 0/8</li>
+            <li>その他 Lv. 7 1/21</li>
+          </ul>
+        </div>
+      </div>
+      
     </div>
   );
 }
